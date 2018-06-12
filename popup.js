a4d4702eba334be37212f9093d5454214761fece
Heino Pärn <heinop2rn@users.noreply.github.com>
2018-06-12 13:51:33 +0300

    Nail extension - Image replacement works on click

---
+++ popup.js
@@ -0,0 +1,5 @@
+window.onload=function() { 
+  chrome.tabs.executeScript({
+    file: 'switch.js'
+  });
+}; 
---
+++ switchFigures.js
@@ -0,0 +1,3 @@
+for (var i = 0; document.figure.length; i++) {
+  document.figure[i].src = "https://thecuddl.com/images/2018/03/03-pretty-chrome-nail-art-thecuddl.jpg"
+};
